ff5a0a1fd47ac95980b768013189e02ac50dcb4e
Ken-Chy129 <1294967895@qq.com>
2023-01-02 11:24:39 +0800

    Initial commit

---
+++ 数组/1.两数之和.java
@@ -0,0 +1,29 @@
+package leetcode.数组;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    // 暴力遍历数组，二重循环，O(n^2)
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    // 
+}
+// @lc code=end
+
