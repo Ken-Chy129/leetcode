32401ac6926d5965ebc4f1ecd608bec7eb6bc348
Ken-Chy129 <1294967895@qq.com>
2023-03-11 13:02:53 +0800

    美团笔试

---
+++ 美团/公司食堂.java
@@ -0,0 +1,19 @@
+package leetcode.美团;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/3/10 23:24
+ */
+public class 公司食堂 {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
---
+++ 美团/正则序列.java
@@ -0,0 +1,37 @@
+package leetcode.美团;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/3/10 22:19
+ */
+public class 正则序列 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int[] nums = new int[20001];
+        for (int i = 0; i < num; i++) {
+            int key = in.nextInt();
+            nums[key + 10000] = nums[key + 10000] + 1;
+        }
+        int regular = 1, ptr = 0, res = 0;
+        while (num >= regular) {
+            if (nums[ptr] == 0) {
+                ptr++;
+                continue;
+            }
+            while (nums[ptr] > 0) {
+                res += Math.abs((ptr - 10000) - regular++);
+                nums[ptr]--;
+            }
+            ptr++;
+        }
+        System.out.println(res);
+    }
+}
