7e5f47771de11c999e5f86e54d3d72342aa921f9
Ken-Chy129 <1294967895@qq.com>
2023-03-18 22:19:50 +0800

    剑指Offer day12

---
+++ 剑指offer/Offer52_两个链表的第一个公共节点.java
@@ -0,0 +1,21 @@
+package leetcode.剑指offer;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/3/18 22:12
+ */
+public class Offer52_两个链表的第一个公共节点 {
+
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA, p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
+}
