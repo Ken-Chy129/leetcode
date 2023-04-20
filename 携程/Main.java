71aac828ae6e60b16ba40a321219d4a34151d393
Ken-Chy129 <1294967895@qq.com>
2023-04-20 15:38:35 +0800

    携程笔试

---
+++ 携程/Main.java
@@ -0,0 +1,46 @@
+package leetcode.携程;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/3/19 22:08
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), m = in.nextInt();
+        char[][] mp = new char[n + 1][m + 1];
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            mp[i] = in.nextLine().toCharArray();
+        }
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < m - 1; j++) {
+                boolean y = false, o = false, u = false;
+                for (int p = 0; p <= 1; p++) {
+                    for (int q = 0; q <= 1; q++) {
+                        if (mp[i + p][j + q] == 'y') {
+                            y = true;
+                        } else if (mp[i + p][j + q] == 'o') {
+                            o = true;
+                        } else if (mp[i + p][j + q] == 'u') {
+                            u = true;
+                        }
+                    }
+                }
+                if (y && o && u) {
+                    res++;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+
+}
---
+++ 携程/Main3.java
@@ -0,0 +1,35 @@
+package leetcode.携程;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/4/15 20:12
+ */
+public class Main3 {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long[] nums = new long[n + 1];
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextLong();
+            res = (long) (((res + sum(nums[i])) % (1e9 + 7)) % (1e9 + 7));
+        }
+        for (int i = 2; i <= n; i+=2) {
+            for (int j = 0; j < n - i; j++) {
+                res = (long) ((res + Math.min(nums[j], nums[j + i])) % (1e9 + 7));
+            }
+        }
+        System.out.println((long) (res % (1e9 + 7)));
+    }
+
+    private static long sum(long n) {
+        return (long) ((((((1 + n) % (1e9 + 7)) * n) % (1e9 + 7)) / 2) % (1e9 + 7));
+    }
+}
