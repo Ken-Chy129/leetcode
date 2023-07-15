64fd911c21aeeb0a72865f4237db29c1f960470b
Ken-Chy129 <1294967895@qq.com>
2023-07-15 11:15:13 +0800

    剑指Offer-查找

---
+++ 暑假修行/查找/二维数组中的查找.java
@@ -0,0 +1,29 @@
+package leetcode.暑假修行.查找;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/7/15 11:10
+ */
+public class 二维数组中的查找 {
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int i = matrix.length - 1, j = 0;
+        while (i >= 0 && j <= matrix[0].length) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
