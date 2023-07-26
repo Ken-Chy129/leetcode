c8fe6ec6f718cdf67539efe36b4491ecaa1c9642
Ken-Chy129 <1294967895@qq.com>
2023-07-27 01:48:20 +0800

    剑指Offer-搜索和回溯

---
+++ 暑假修行/搜索和回溯/二叉搜索树的最近公共祖先.java
@@ -0,0 +1,24 @@
+package leetcode.暑假修行.搜索和回溯;
+
+import leetcode.分类刷题.树.TreeNode;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/7/26 19:55
+ */
+public class 二叉搜索树的最近公共祖先 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
---
+++ 暑假修行/搜索和回溯/二叉树的最近公共祖先.java
@@ -0,0 +1,32 @@
+package leetcode.暑假修行.搜索和回溯;
+
+import leetcode.分类刷题.树.TreeNode;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author <a href="https://github.com/Ken-Chy129">Ken-Chy129</a>
+ * @since 2023/7/26 20:08
+ */
+public class 二叉树的最近公共祖先 {
+
+    boolean left, right;
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+    
+}
